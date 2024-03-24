5f46f79efd2f83251a0be67acc7872bbc57f5896
Alex <alexandercastronovo@gmail.com>
2024-03-24 19:25:05 -0400

    add flask backend

---
+++ app.py
@@ -0,0 +1,14 @@
+from waitress import serve
+from flask import Flask, render_template
+
+app = Flask(__name__)
+# TODO: Add the SECRET_KEY configuration to the app object.
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    serve(app, host="localhost", port=5000)
