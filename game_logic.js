be9998a35520fc18132cddef8c3c242fa23cfbc6
Alex <alexandercastronovo@gmail.com>
2024-03-23 23:30:32 -0400

    selection logic + box listener

--- game_logic.js
+++ game_logic.js
@@ -1,10 +1,51 @@
 sudokuBoard = Array(81).fill(0);
 
 
+
+function boxClicked(e) {
+    highlightBoxes(parseInt(e.target.id));
+}
+
+/**
+ * Highlights the boxes that are in the same row, column, 3x3 grid, or are the same number as the box with the given id.
+ */
+function highlightBoxes(boxID) {
+    let boxes = Array.from(document.getElementsByClassName("box"));
+    let [row, col] = getRowCol(boxID);
+    let grid = getGridTopLeft(boxID);
+    let num = sudokuBoard[boxID];
+
+    for (let i = 0; i < 81; i++) {
+        let [row2, col2] = getRowCol(i);
+        let grid2 = getGridTopLeft(boxID);
+
+        if (row === row2 || col === col2 || grid === grid2 || num === sudokuBoard[i]) {
+            boxes[i].classList.add("highlight");
+            console.log("highlighting box " + i);
+        }
+    }
+}
+
+function getGridTopLeft(boxID) {
+    let [row, col] = getRowCol(boxID);
+    let gridRow = Math.floor(row / 3) * 3;
+    let gridCol = Math.floor(col / 3) * 3;
+    return gridRow * 9 + gridCol;
+}
+
+function getRowCol(boxID) {
+    let boxes = Array.from(document.getElementsByClassName("box"));
+    let row = Math.floor(boxID / 9);
+    let col = boxID % 9;
+    return [row, col];
+}
+
+
 function updateBoard() {
     let boxes = Array.from(document.getElementsByClassName("box"));
+
     for (let i = 0; i < boxes.length; i++) {
-        if (sudokuBoard[i] !== 1) {
+        if (sudokuBoard[i] !== 0) {
             boxes[i].innerText = sudokuBoard[i].toString();
         } else {
             boxes[i].innerText = "";
@@ -23,9 +64,20 @@ function genGrid() {
     }
 }
 
+
+function getBoxes() {
+    return Array.from(document.getElementsByClassName("box"));
+
+}
+
+
 function init() {
     genGrid();
     updateBoard();
+
+    getBoxes().forEach((box) => {
+        box.addEventListener("click", boxClicked);
+    })
 }
 
 window.onload = init;
