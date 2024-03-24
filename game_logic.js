a7e9b3ff3027dd32a99d3767432c8da780681815
Alex <alexandercastronovo@gmail.com>
2024-03-24 13:57:46 -0400

    wrong color is added to the board if incorrect number is added

--- game_logic.js
+++ game_logic.js
@@ -1,5 +1,6 @@
 let sudokuBoard = Array(81).fill(0);
-sudokuBoard = Array.from({length: 81}, () => Math.floor(Math.random() * 10));
+sudokuBoard = [2, 6, 4, 3, 9, 1, 7, 8, 5, 3, 1, 5, 6, 7, 0, 9, 2, 0, 7, 9, 8, 4, 2, 5, 6, 3, 1, 4, 8, 3, 2, 1, 0, 5, 9, 6, 9, 0, 6, 0, 4, 3, 2, 0, 8, 5, 0, 1, 8, 6, 9, 3, 4, 7, 6, 3, 2, 0, 5, 0, 8, 7, 9, 0, 5, 7, 9, 8, 2, 4, 6, 3, 8, 4, 9, 7, 3, 6, 1, 5, 2];
+let correctBoard = [2, 6, 4, 3, 9, 1, 7, 8, 5, 3, 1, 5, 6, 7, 8, 9, 2, 4, 7, 9, 8, 4, 2, 5, 6, 3, 1, 4, 8, 3, 2, 1, 7, 5, 9, 6, 9, 7, 6, 5, 4, 3, 2, 1, 8, 5, 2, 1, 8, 6, 9, 3, 4, 7, 6, 3, 2, 1, 5, 4, 8, 7, 9, 1, 5, 7, 9, 8, 2, 4, 6, 3, 8, 4, 9, 7, 3, 6, 1, 5, 2];
 let selectedBox = -1;
 
 function boxClicked(e) {
@@ -30,12 +31,27 @@ function onKeyPress(e) {
 
 
 /**
+ * Checks if the board is correct. A box isn't correct, it colors it with wrongColor.
+ */
+function addWrongColor() {
+    let boxes = getBoxes();
+    for (let i = 0; i < 81; i++) {
+        if (sudokuBoard[i] !== 0 && sudokuBoard[i] !== correctBoard[i]) {
+            boxes[i].classList.add("wrongColor");
+        } else {
+            boxes[i].classList.remove("wrongColor");
+        }
+    }
+}
+
+
+/**
  * Highlights the boxes that are in the same row, column, 3x3 grid, or are the same number as the box with the given id.
  */
 function highlightBoxes(boxID) {
     unhighlightBoxes();
 
-    let boxes = Array.from(document.getElementsByClassName("box"));
+    let boxes = getBoxes();
     let [row, col] = getRowCol(boxID);
     let gridTopLeft = getGridTopLeft(boxID);
     let num = sudokuBoard[boxID];
@@ -81,6 +97,7 @@ function getRowCol(boxID) {
  */
 function updateBoard() {
     updateBoxText();
+    addWrongColor();
     highlightBoxes(selectedBox);
 }
 
