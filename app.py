d9fa2fd88c3905d18f1faa06a8fd8094882f0528
Alex <alexandercastronovo@gmail.com>
2024-03-24 20:41:32 -0400

    OMG MULTIPLAYER YES LETS GO THIS IS INSANE OMG OMG OM GOMG OMGOMGOGMGOMGOMGOGM

--- app.py
+++ app.py
@@ -1,14 +1,30 @@
-from waitress import serve
 from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+
 
 app = Flask(__name__)
 # TODO: Add the SECRET_KEY configuration to the app object.
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 
-@app.route('/')
-def hello_world():
+@app.route("/")
+def index():
     return render_template("index.html")
 
 
+@socketio.on("updateBoard")
+def handle_message(message):
+    print(f"Received message: {message}")
+    emit("updateBoard", message, broadcast=True)
+
+
+@socketio.on("connect")
+def handle_connect():
+    # send the correct board to the client when they connect. the correct board is [0] * 81
+    emit("correctBoard", [1] * 81)
+    emit("updateBoard", [0] * 81)
+    print("Received connection")
+
+
 if __name__ == "__main__":
-    serve(app, host="localhost", port=5000)
+    socketio.run(app, host="localhost", use_reloader=False, log_output=True)
--- static/game_logic.js
+++ static/game_logic.js
@@ -2,6 +2,7 @@ let sudokuBoard = Array(81).fill(0);
 sudokuBoard = [2, 6, 4, 3, 9, 1, 7, 8, 5, 3, 1, 5, 6, 7, 0, 9, 2, 0, 7, 9, 8, 4, 2, 5, 6, 3, 1, 4, 8, 3, 2, 1, 0, 5, 9, 6, 9, 0, 6, 0, 4, 3, 2, 0, 8, 5, 0, 1, 8, 6, 9, 3, 4, 7, 6, 3, 2, 0, 5, 0, 8, 7, 9, 0, 5, 7, 9, 8, 2, 4, 6, 3, 8, 4, 9, 7, 3, 6, 1, 5, 2];
 let correctBoard = [2, 6, 4, 3, 9, 1, 7, 8, 5, 3, 1, 5, 6, 7, 8, 9, 2, 4, 7, 9, 8, 4, 2, 5, 6, 3, 1, 4, 8, 3, 2, 1, 7, 5, 9, 6, 9, 7, 6, 5, 4, 3, 2, 1, 8, 5, 2, 1, 8, 6, 9, 3, 4, 7, 6, 3, 2, 1, 5, 4, 8, 7, 9, 1, 5, 7, 9, 8, 2, 4, 6, 3, 8, 4, 9, 7, 3, 6, 1, 5, 2];
 let selectedBox = -1;
+let socket = io.connect("http://localhost:5000");
 
 function boxClicked(e) {
     selectedBox = parseInt(e.target.id);
@@ -16,17 +17,16 @@ function onKeyPress(e) {
 
     if (e.key === "Backspace" || e.key === "Delete") {
         sudokuBoard[selectedBox] = 0;
-        updateBoard();
-        return;
+    } else {
+        let num = parseInt(e.key);
+        if (!isNaN(num) && num >= 1 && num <= 9) {
+            sudokuBoard[selectedBox] = num;
+        }
     }
 
-    let num = parseInt(e.key);
-    if (isNaN(num) || num < 1 || num > 9) {
-        return;
-    }
-
-    sudokuBoard[selectedBox] = num;
     updateBoard();
+
+    socket.emit("updateBoard", sudokuBoard);
 }
 
 
@@ -141,6 +141,30 @@ function init() {
     })
 
     document.addEventListener("keydown", onKeyPress);
+
+
+    socket.on("connect", () => {
+        console.log("Connected to server");
+    })
+
+    socket.on("disconnect", () => {
+        console.log("Disconnected from server");
+    })
+
+    socket.on("connect_error", (error) => {
+        console.log("CONNECT ERROR: " + error);
+    })
+
+    socket.on("correctBoard", (data) => {
+        console.log("Received correct board: " + data);
+        correctBoard = data;
+    })
+
+    socket.on("updateBoard", (data) => {
+        console.log("Received updated board: " + data);
+        sudokuBoard = data;
+        updateBoard();
+    })
 }
 
 window.onload = init;
