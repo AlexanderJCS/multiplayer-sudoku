132dda083cf054ab9b03010d7a47f10a51ef164d
Alex <alexandercastronovo@gmail.com>
2024-03-26 08:45:07 -0400

    Update instructions of generate.py

--- puzzle-generator/generate.py
+++ puzzle-generator/generate.py
@@ -6,6 +6,8 @@ be used by the Flask app to serve puzzles to the user. The database will have a 
 """
 
 import sqlite3
+import time
+
 from sudoku_board import Board
 
 
@@ -40,16 +42,17 @@ def flatten(arr: list[list[int]]) -> list[int]:
     return [item for sublist in arr for item in sublist]
 
 
-def generate_puzzles(cursor):
+def generate_puzzles(num_puzzles, conn, cursor):
     """
     Generate puzzles and add them to the database
 
+    :param num_puzzles: The number of puzzles to generate
+    :param conn: The database connection object to use
     :param cursor: The database cursor object to use
     """
     
     board = Board()
     
-    num_puzzles = 1
     for i in range(num_puzzles):
         print(f"Generating puzzle {i + 1} / {num_puzzles}: {i / num_puzzles * 100:.2f}%")
         
@@ -57,18 +60,32 @@ def generate_puzzles(cursor):
         solution = board.generate()
 
         add_row(cursor, flatten(board.board), flatten(solution))
+        conn.commit()
 
 
 def main():
+    print("""
+This program will generate Sudoku puzzles and append them to an SQLite database named puzzles.db.
+The database file may be moved to the repository's root directory to be used by the Flask app.
+
+This may take a while to run depending on your system, depending on CPU speed, memory size, and number of puzzles.
+On my system (Intel i7-12700KF, 16 GB DDR4 RAM), it took about 45 seconds to generate 10 puzzles
+    """)
+    
+    num_puzzles = int(input("Enter the number of puzzles to generate: "))
+    
+    start = time.time()
     conn = sqlite3.connect("puzzles.db")
     cursor = conn.cursor()
     create_table(cursor)
     conn.commit()
     
-    generate_puzzles(cursor)
+    generate_puzzles(num_puzzles, conn, cursor)
     conn.commit()
     
     conn.close()
+    
+    print(f"Completed in {time.time() - start:.2f} seconds. See the database at ./puzzles.db.")
 
 
 if __name__ == "__main__":
