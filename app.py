067b475015a46124990eb4b2a6dfc2466d1d4b4d
Alex <alexandercastronovo@gmail.com>
2024-03-26 21:25:22 -0400

    change ip to 0.0.0.0

--- app.py
+++ app.py
@@ -41,4 +41,4 @@ def handle_connect():
 
 
 if __name__ == "__main__":
-    socketio.run(app, host="localhost", use_reloader=False, log_output=True)
+    socketio.run(app, host="0.0.0.0", use_reloader=False, log_output=True)
