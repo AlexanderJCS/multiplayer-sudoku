79de901abcbcab54e22b708f70ce857f39116d15
Alex <alexandercastronovo@gmail.com>
2024-04-08 19:53:35 -0400

    Add enter key shortcut for clicking the "join" button

--- static/homepage/join.js
+++ static/homepage/join.js
@@ -1,4 +1,9 @@
-
+document.getElementById('gameId').addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        event.preventDefault(); // Prevent form submission
+        document.getElementById('join').click();
+    }
+});
 
 document.getElementById("join").addEventListener("click", function() {
     window.location.href = "/" + document.getElementById("gameId").value;
